0d6607aef14b269bd72c1021532cadc8d060fbc4
soberich <25544967+soberich@users.noreply.github.com>
2022-02-25 16:54:00 +0100

    micronaut-projects/micronaut-serialization#134 failing version

--- src/main/java/com/example/Pojo.java
+++ src/main/java/com/example/Pojo.java
@@ -7,6 +7,7 @@ import io.micronaut.serde.annotation.Serdeable;
 public final class Pojo {
     private final String name;
     @Nullable
+    @jakarta.persistence.GeneratedValue // <- N.B.
     private final String surname;
 
     public Pojo(String name, @Nullable String surname) {
